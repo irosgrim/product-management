374b593481fff04bfad8d9fa4106930c4bb7397a
ion <irosgrim@gmail.com>
2021-04-22 12:23:56 +0200

    uid credits

--- services/server/src/helpers/uid.ts
+++ services/server/src/helpers/uid.ts
@@ -1,3 +1,5 @@
+// https://github.com/lukeed/uid
+
 let IDX=256; 
 let HEX: string[]=[];
 let SIZE=256; 
@@ -5,7 +7,8 @@ let BUFFER: string;
 while (IDX--) HEX[IDX] = (IDX + 256).toString(16).substring(1);
 
 export function uid(len?: number): string {
-  var i=0, tmp=(len || 11);
+  let i=0;
+  let tmp=(len || 11);
   if (!BUFFER || ((IDX + tmp) > SIZE*2)) {
     for (BUFFER='',IDX=0; i < SIZE; i++) {
       BUFFER += HEX[Math.random() * 256 | 0];
